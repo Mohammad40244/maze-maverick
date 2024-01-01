6c29a96db65deb337b642ebe58a01f25b837c3ef
Mohammad40244 <152492819+Mohammad40244@users.noreply.github.com>
2024-01-01 09:50:16 +0330

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main()
+{
+    srand(time(0));
+int x,y;
+cin>>x>>y;
+
+ {cout<<char(218);
+ int aa=7;
+       for(int i=1;i<y;i++)
+       {
+        int a=4;
+        while (a--)
+        {
+          cout<<char(196);
+       }
+         cout<<char(194);
+        }
+          
+           int a=4;
+        while (a--)
+        {
+            cout<<char(196);
+        }
+        cout<<char(191);}
+ cout<<endl;
+ for(int i=1;i<=x;i++)
+{
+ 
+ 
+ {cout<<char(179);
+ 
+ for(int j=1;j<=y;j++)
+ {
+ 
+  int a=4;
+        while (a--)
+        {cout<<" ";
+           // cout<<char(178);//white
+        }
+cout<<char(179);
+         a=4;
+        /*while (a--)
+        {cout<<" ";
+           // cout<<char(176);//black
+        }*/
+//cout<<char(179);
+ }}
+cout<<endl;
+   { cout<<char(179);
+
+
+for(int i=1;i<=y;i++)
+     {
+       int random_number = rand() % 7 - 3;
+        if (random_number >= 0) {
+            cout << "  " << random_number<<" "<<char(179);
+        } else {
+            cout<<" " << random_number<<" "<<char(179);
+        }
+        //cout<<"  "<<rand()% 10<<" "<<char(179);
+      
+      
+        //cout<<"    "<<char(179);
+//cout<<char(178)/*white*/<<"  "<<char(178)<<char(179);
+//cout<<char(176)/*black*/<<"  "<<char(176)<<char(179);
+     }
+
+  }
+   cout<<endl;
+   {cout<<char(179);
+ 
+ for(int i=1;i<=y;i++)
+ {
+ 
+  int a=4;
+        while (a--)
+        {    
+            cout<<" ";
+            //cout<<char(178);//white
+        }
+cout<<char(179);
+         a=4;
+       /* while (a--)
+        {
+            cout<<" ";
+           // cout<<char(176);//black
+        }
+cout<<char(179);*/
+ }}
+cout<<endl;
+{cout<<char(195);
+
+for(int i=1;i<=y-1;i++)
+ 
+cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(197);
+cout<<char(196)<<char(196)<<char(196)<<char(196)<<char(180);
+
+}
+cout<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+}
